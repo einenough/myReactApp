8be70751a841d3f8fc7dcef21d4ff445773a89c8
Arseniy <seny_2978@mail.ru>
2022-12-16 01:14:11 +0500

    tarining react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import "../src/styles/App.css";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 function App() {
-  const [likes, setLikes] = useState(5);
-  const [value, setValue] = useState("any text");
-  const onChangeName = (event) => {
-    console.log(event);
-  };
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "Python", body: "Description" },
+    { id: 3, title: "Java", body: "Description" },
+  ]);
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: "С++", body: "Description" },
+    { id: 2, title: "HTML", body: "Description" },
+    { id: 3, title: "CSS", body: "Description" },
+  ]);
 
   return (
     <div className="App">
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
+      <PostList posts={posts} title={posts.title} />
+      <PostList posts={posts2} title={posts2.title} />
     </div>
   );
 }
