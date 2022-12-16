8beef2bd60d1f6cfbb6eb724e38c16f41cc0eae5
Arseniy <seny_2978@mail.ru>
2022-12-16 19:04:38 +0500

    comeback to this project later

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import "../src/styles/App.css";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "JavaScript", body: "Description" },
     { id: 2, title: "Python", body: "Description" },
     { id: 3, title: "Java", body: "Description" },
   ]);
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: "С++", body: "Description" },
-    { id: 2, title: "HTML", body: "Description" },
-    { id: 3, title: "CSS", body: "Description" },
-  ]);
+
+  const [title, setTitle] = useState("asfasf");
+
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    console.log(title);
+    console.log(bodyInputRef.current.value);
+  };
 
   return (
     <div className="App">
+      <form>
+        <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"
+        />
+        <input ref={bodyInputRef} type="text" />
+
+        {/* <MyInput 
+          ref={bodyInputRef}
+          type="text"
+          placeholder="Описание поста"
+          /> */}
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title={posts.title} />
-      <PostList posts={posts2} title={posts2.title} />
     </div>
   );
 }
